df6f4a9b2c61de061f49ea147fe537b349b6f845
Mohammad sarairha <mohammadsarairha@gmail.com>
2022-03-15 22:06:46 +0300

    Add Score function

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,9 +4,34 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        public static int Scoring(int[] arr, int num)
+        {
+            int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == num)
+                {
+                    count++;
+                }
+            }
+
+            return num * count;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please chosee number");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int [size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Please enter number  {i+1} / {arr.Length}");
+                arr [i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine($"Please choose number {string.Join(",", arr)}");
+            int num = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"Your score is {Scoring(arr, num)}");
         }
     }
 }
