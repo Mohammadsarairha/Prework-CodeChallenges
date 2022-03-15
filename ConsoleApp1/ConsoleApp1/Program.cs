467be92377a6e500c927edaffe6613f5ef5ed1ea
Mohammad sarairha <mohammadsarairha@gmail.com>
2022-03-15 22:24:06 +0300

    Add Leap function

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -19,19 +19,23 @@ namespace ConsoleApp1
             return num * count;
         }
 
+        public static void LeapYear(int year)
+        {
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            {
+                Console.WriteLine($"{year} is Leap year ");
+            }
+            else
+            {
+                Console.WriteLine($"{year} is not Leap year ");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Please chosee number");
-            int size = Convert.ToInt32(Console.ReadLine());
-            int[] arr = new int [size];
-            for (int i = 0; i < size; i++)
-            {
-                Console.WriteLine($"Please enter number  {i+1} / {arr.Length}");
-                arr [i] = Convert.ToInt32(Console.ReadLine());
-            }
-            Console.WriteLine($"Please choose number {string.Join(",", arr)}");
-            int num = Int32.Parse(Console.ReadLine());
-            Console.WriteLine($"Your score is {Scoring(arr, num)}");
+            Console.WriteLine("Please enter year");
+            int year = Int32.Parse(Console.ReadLine());
+            LeapYear(year);
         }
     }
 }
